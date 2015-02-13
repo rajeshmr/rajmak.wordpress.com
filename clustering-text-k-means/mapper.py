fb27e4c7c873b9cca0d03872527e604abd4cce4c
Rajesh Manikka <raj@indix.com>
2015-02-13 12:38:05 +0530

    adding mapper

---
+++ clustering-text-k-means/mapper.py
@@ -0,0 +1,20 @@
+__author__ = 'raj'
+
+import sys
+import re
+
+
+words = list()
+for word in open("bag_of_words.txt").readlines():
+    words.append(word.strip())
+
+for title in sys.stdin:
+    vector = []
+    title = re.sub(r"[^a-zA-Z]", " ", title).lower()
+    words_in_title = map(lambda x: x.strip(), title.split())
+    words_in_title = filter(lambda x: len(x) > 2, words_in_title)
+
+    for word in words:
+        count = len(filter(lambda x: x in word, words_in_title))
+        vector.append(float(count))
+    print vector
