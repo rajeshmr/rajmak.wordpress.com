5412a7238466363631b5aa87bf704d26f64d07d3
Rajesh Manikka <rraj19@gmail.com>
2015-09-14 17:56:04 +0530

    wiring thrift client to pipeline

--- thrift-data-pipeline/crawler/settings.py
+++ thrift-data-pipeline/crawler/settings.py
@@ -4,3 +4,6 @@ SPIDER_MODULES = ['crawler.spiders']
 NEWSPIDER_MODULE = 'crawler.spiders'
 USER_AGENT = 'sloth_crawler (+http://rajmak.wordpress.com/)'
 
+ITEM_PIPELINES = {
+    'crawler.pipeline.ThriftClientPipeline': 10
+}
