9c5be47378bd5ba8ace739720ce530b6389a3486
Rajesh Manikka <rraj19@gmail.com>
2015-09-14 17:52:58 +0530

    adding scrapy as dependency

--- thrift-data-pipeline/setup.py
+++ thrift-data-pipeline/setup.py
@@ -3,5 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="thrift-data-pipeline",
     version="0.1",
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=[
+        "scrapy"
+    ]
 )
