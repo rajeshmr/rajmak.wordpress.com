ef7595b95eb79bdc1d14b68443c0c2c437a61e36
Rajesh Manikka <rraj19@gmail.com>
2015-09-14 17:11:47 +0530

    adding configs and modules

---
+++ thrift-data-pipeline/clean.sh
@@ -0,0 +1,1 @@
+rm -rf bin/ build/ cover/ develop-eggs/ dist/ eggs/ normalizer.egg-info/ parts/ .coverage .installed.cfg .DS_Store
---
+++ thrift-data-pipeline/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="thrift-data-pipeline",
+    version="0.1",
+    packages=find_packages()
+)
