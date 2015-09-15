93e59ba91292cae22aa90866bba8cdbdc5d9c3f6
Rajesh Manikka <raj@indix.com>
2015-09-15 15:23:59 +0530

    moving pipeline related config to separate file

--- thrift-data-pipeline/crawler/settings.py
+++ thrift-data-pipeline/crawler/settings.py
@@ -7,6 +7,4 @@ USER_AGENT = 'rajmak-bot (+http://rajmak.wordpress.com/)'
 ITEM_PIPELINES = {
     'crawler.pipeline.ThriftClientPipeline': 10
 }
-DOWNLOAD_DELAY = 0.25  # 250 ms of delay
-PARSER_SERVICE_HOST = 'localhost'
-PARSER_SERVICE_PORT = 8090
+DOWNLOAD_DELAY = 1
