4ae1901d6f6209c52528083241a8221a0f1eb3f5
Rajesh Manikka <raj@indix.com>
2015-09-15 11:41:22 +0530

    updating bot info

--- thrift-data-pipeline/crawler/settings.py
+++ thrift-data-pipeline/crawler/settings.py
@@ -1,9 +1,12 @@
-BOT_NAME = 'sloth_crawler'
+BOT_NAME = 'rajmak-bot'
 
 SPIDER_MODULES = ['crawler.spiders']
 NEWSPIDER_MODULE = 'crawler.spiders'
-USER_AGENT = 'sloth_crawler (+http://rajmak.wordpress.com/)'
+USER_AGENT = 'rajmak-bot (+http://rajmak.wordpress.com/)'
 
 ITEM_PIPELINES = {
     'crawler.pipeline.ThriftClientPipeline': 10
 }
+DOWNLOAD_DELAY = 0.25  # 250 ms of delay
+PARSER_SERVICE_HOST = 'localhost'
+PARSER_SERVICE_PORT = 8090
