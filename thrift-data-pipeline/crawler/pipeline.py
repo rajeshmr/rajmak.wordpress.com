04ed104fe2dc5a92e6b5811aa9782bd38b8a76bd
Rajesh Manikka <raj@indix.com>
2015-09-15 11:40:30 +0530

    placeholder for thrift client

--- thrift-data-pipeline/crawler/pipeline.py
+++ thrift-data-pipeline/crawler/pipeline.py
@@ -1,4 +1,11 @@
 
+class ThriftItemHandler:
+    pass
+
 class ThriftClientPipeline(object):
+    def __init__(self):
+        # check for parser service
+        pass
+
     def process_item(self, item, spider):
         return item
