7795882f8323c6b9e01ed5b61d8c04959c19a201
Rajesh Manikka <raj@indix.com>
2015-09-15 15:24:44 +0530

    adding parser client to crawler

--- thrift-data-pipeline/crawler/pipeline.py
+++ thrift-data-pipeline/crawler/pipeline.py
@@ -1,11 +1,26 @@
+from thrift import Thrift
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+from models import ParserService
+import pipeline_config
 
-class ThriftItemHandler:
-    pass
 
 class ThriftClientPipeline(object):
+    client = None
+
     def __init__(self):
-        # check for parser service
-        pass
+        try:
+            socket = TSocket.TSocket(pipeline_config.PARSER_SERVICE_HOST, pipeline_config.PARSER_SERVICE_PORT)
+            transport = TTransport.TBufferedTransport(socket)
+            protocol = TBinaryProtocol.TBinaryProtocol(transport)
+            self.client = ParserService.Client(protocol)
+            transport.open()
+            self.client.ping()
+        except Thrift.TException, tx:
+            print "%s" % tx.message
+            exit("Parser service not running!")
 
     def process_item(self, item, spider):
-        return item
+        self.client.parse(item['body'])
+        return item['url']
