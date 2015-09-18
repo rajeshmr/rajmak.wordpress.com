2ecffb5442ba4cc99ea9e466dd2a6a3eb18d7605
Rajesh Manikka <rraj19@gmail.com>
2015-09-18 11:58:03 +0530

    wiring get_model utils method

--- thrift-data-pipeline/extractor/handler.py
+++ thrift-data-pipeline/extractor/handler.py
@@ -1,18 +1,19 @@
 from models.ttypes import Product
 from scrapy.selector import Selector
 from urlparse import urlparse
+from utils import thrift_utils
 
 
 class ParserServiceHandler:
     def __init__(self):
         self.parser_meta = {
-            "amazon.in":{
-                "title": "//div[@id='titleSection']/h1[@id='title']/span[@id='productTitle']",
-                "price": "//div[@id='price']/table[@class='a-lineitem']/tbody/tr/td/span[@id='priceblock_saleprice']"
+            "amazon.in": {
+                "title": "//div[@id='titleSection']/h1[@id='title']/span[@id='productTitle']/text()",
+                "price": "//div[@id='price']/table[@class='a-lineitem']/tbody/tr/td/span[@id='priceblock_saleprice']/text()"
             },
-            "flipkart.com":{
-                "title": "//div[@class='title-wrap line fk-font-family-museo section omniture-field']/h1[@class='title']",
-                "price": ""
+            "flipkart.com": {
+                "title": "//div[@class='title-wrap line fk-font-family-museo section omniture-field']/h1/text()",
+                "price": "//div[@class='prices']/div/span[@class='selling-price omniture-field']/text()"
             }
         }
 
@@ -20,10 +21,9 @@ class ParserServiceHandler:
         print "ping"
 
     def parse(self, html):
+        data = dict()
         selector = Selector(text=html.html)
-        parsed_url = urlparse(html.url)
-        # for key, selector in self.parser_meta[parsed_url.netloc]:
-
-        title = "Hello World"
-        price = 100.00
-        return Product(title=title, price=price)
+        domain = urlparse(html.url).netloc
+        for key, xpath in self.parser_meta[domain]:
+            data[key] = selector.xpath(xpath)
+        return thrift_utils.get_model(data, Product)
