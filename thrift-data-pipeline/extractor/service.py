490454fc1513cf02618becd6174ff83d7c92f9a1
Rajesh Manikka <rraj19@gmail.com>
2015-09-18 16:22:08 +0530

    parser handler changes

--- thrift-data-pipeline/extractor/service.py
+++ thrift-data-pipeline/extractor/service.py
@@ -1,19 +1,11 @@
 from models import ParserService
-from thrift.transport import TSocket
-from thrift.transport import TTransport
-from thrift.protocol import TBinaryProtocol
-from thrift.server import TServer
 import pipeline_config
-from handler import ParserServiceHandler
+from extractor.handler import ParserServiceHandler
+from utils import thrift_utils
 
 if __name__ == '__main__':
-    handler = ParserServiceHandler()
-    processor = ParserService.Processor(handler)
-    transport = TSocket.TServerSocket(port=pipeline_config.PARSER_SERVICE_PORT)
-    tfactory = TTransport.TBufferedTransportFactory()
-    pfactory = TBinaryProtocol.TBinaryProtocolFactory()
-
-    server = TServer.TSimpleServer(processor, transport, tfactory, pfactory)
+    parser_handler = ParserServiceHandler()
+    server = thrift_utils.get_server(ParserService, parser_handler, pipeline_config.PARSER_SERVICE_PORT)
     print "Starting parser server"
     server.serve()
     print "Terminating parser server"
