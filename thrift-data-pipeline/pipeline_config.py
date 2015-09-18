d805c2626b1b3c47c66520e66cbe9681ed3c7111
Rajesh Manikka <rraj19@gmail.com>
2015-09-18 16:21:03 +0530

    adding pipeline configs

--- thrift-data-pipeline/pipeline_config.py
+++ thrift-data-pipeline/pipeline_config.py
@@ -1,6 +1,12 @@
+import os
 
 PARSER_SERVICE_HOST = 'localhost'
 PARSER_SERVICE_PORT = 8090
 
 WRITER_SERVICE_HOST = 'localhost'
 WRITER_SERVICE_PORT = 8091
+
+PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
+DB_LOCATION = "%s/%s" % (PROJECT_DIR, "crawl.db")
+DB_SCHEMA = "%s/%s" % (PROJECT_DIR, "schema.sql")
+JSON_DUMP = "%s/%s" % (PROJECT_DIR, "crawl.jsonline")
