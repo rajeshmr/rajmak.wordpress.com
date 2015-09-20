9379c2fdb18427f0ed9fc6f2bf09936e34befd7d
Rajesh Manikka <raj@indix.com>
2015-09-20 19:56:04 +0530

    processing head of value

--- thrift-data-pipeline/extractor/service.py
+++ thrift-data-pipeline/extractor/service.py
@@ -1,6 +1,4 @@
 import logging
-from logging.config import fileConfig
-import sys
 
 from models import ParserService
 import pipeline_config
@@ -8,7 +6,6 @@ from extractor.handler import ParserServiceHandler
 from utils import thrift_utils
 
 if __name__ == '__main__':
-    fileConfig()
     parser_handler = ParserServiceHandler()
     server = thrift_utils.get_server(ParserService, parser_handler, pipeline_config.PARSER_SERVICE_PORT)
     logging.info("Starting parser server")
--- thrift-data-pipeline/utils/thrift_utils.py
+++ thrift-data-pipeline/utils/thrift_utils.py
@@ -10,22 +10,24 @@ def get_model(data, thrift):
     spec = thrift.thrift_spec
     spec = filter(lambda x: x is not None, spec)
     for tid, ttype, field, _, _ in spec:
-        value = data[field]
-        value = value.strip()
+        if field in data:
+            value = data[field]
+            if value:
+                value = value[0]
 
-        if ttype is TType.BOOL:
-            value = bool(value)
-        elif ttype is TType.DOUBLE:
-            value = ''.join([i for i in value if i.isdigit() or i is "."])
-            value = float(value)
-        elif ttype is TType.I16 or ttype is TType.I32 or ttype is TType.I64:
-            value = ''.join([i for i in value if i.isdigit()])
-            value = int(value)
-        elif ttype is TType.STRING:
-            value = str(value)
-        else:
-            pass
-        result.append((tid, value))
+            if ttype is TType.BOOL:
+                value = bool(value)
+            elif ttype is TType.DOUBLE:
+                value = ''.join([i for i in value if i.isdigit() or i is "."])
+                value = float(value)
+            elif ttype is TType.I16 or ttype is TType.I32 or ttype is TType.I64:
+                value = ''.join([i for i in value if i.isdigit()])
+                value = int(value)
+            elif ttype is TType.STRING:
+                value = str(value)
+            else:
+                pass
+            result.append((tid, value))
     result = sorted(result)
     result = map(lambda x: x[1], result)
     return thrift(*result)
