86c3890f0857cfa5d6e75b55c39a6acc39df0464
Rajesh Manikka <raj@indix.com>
2016-02-15 14:46:00 +0530

    write to cli instead of file

--- structuring-text/crf_input_generator.py
+++ structuring-text/crf_input_generator.py
@@ -2,16 +2,14 @@ import sys
 import nltk
 import json
 
-with open("token_pos.txt", "w") as f:
-    for line in sys.stdin:
-        data = json.loads(line)
-        for ingredient in data['ingredients']:
-            tokens = nltk.word_tokenize(ingredient.strip())
-            tagged_tokens = nltk.pos_tag(tokens)
-            for token, pos in tagged_tokens:
-                try:
-                    f.write("%s %s\n" % (token.encode('utf8'), pos))
-                except:
-                    print token
-            f.write("\n")
-f.close()
+
+for line in sys.stdin:
+    data = json.loads(line)
+    for ingredient in data['ingredients']:
+        tokens = nltk.word_tokenize(ingredient.strip())
+        tagged_tokens = nltk.pos_tag(tokens)
+        for token, pos in tagged_tokens:
+            try:
+                print "%s\t%s" % (token.encode('utf8'), pos)
+            except:
+                print "Error writing token:", token
